29fcb5d60fc1c83f33dcd0da8c330381dedfdfd7
Dongkun Lee <ldkun2001@gmail.com>
2024-11-18 13:43:05 -0700

    Updating Host

--- BasicMachineLearning/settings.py
+++ BasicMachineLearning/settings.py
@@ -34,7 +34,14 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'basicmachinlearning.onrender.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://basicmachinlearning.onrender.com']
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    'localhost:8000',
+]
 
 
 # Application definition
